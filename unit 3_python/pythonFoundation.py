b1a1424c529f2a73e63227f0fb2ef768623e514d
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-10-31 12:46:48 +0000

    learned python fundimentals

---
+++ unit 3_python/pythonFoundation.py
@@ -0,0 +1,36 @@
+# data types= are the fundamental building 
+# blocks of python
+
+# intergers= whole nubers
+#string= any characters wrapped in quotes
+#boolean= true or false
+# we output data using the print function
+print(200 * 8)
+print('welcome to python')
+print(True)
+
+() #round brackets = function
+{} #curly brackets= objects 
+[]#sqaure brackets=lists
+
+#variables=container for data
+# we write variables by first delclaring/ giving it a name 
+# we use the equal sign
+# we then give it data
+name='James'
+message='good morning'
+
+# concatenation = combining strings
+print(message, name)
+
+story = " I will be going to"
+location='the park'
+time ='at 5:30'
+print(story,location, time)
+
+valA=10
+valB=20
+
+print(valA,valB)
+
+
