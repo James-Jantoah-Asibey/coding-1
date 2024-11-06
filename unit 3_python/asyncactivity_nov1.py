14c2aa9c472817590ea9bd411b420824533fcda2
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-11-06 23:26:05 +0000

    I finished typing the notes

--- unit 3_python/asyncactivity_nov1.py
+++ unit 3_python/asyncactivity_nov1.py
@@ -1,6 +1,8 @@
 a=200
 b=45
 
-print(a+b)
+print(a+b) 
 
-"a list would be best to store allthe data"
+"A list would be best to store all the data"
+"I would use the integer data type for the numeric datas of the week"
+"I would use the string data type for the word/ string days of the week"
