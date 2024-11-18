e28ce9cfc311f1bc564f1a4f127b1e0f20c92838
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-11-18 13:34:19 +0000

    this is thte right one

--- unit 3_python/asyncActivityNov11.py
+++ unit 3_python/asyncActivityNov11.py
@@ -1,10 +1,12 @@
 #1. A company is developing a security system that requires 2 factor authorization. This means that the system needs to verify that 2 pieces of data are correct for the person to enter the building. When someone approached the building they need to have the correct name and correct passcode to enter the building. 
 #Which operator would be used here? Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript? 
 
-# I think we would have to use Arthmatic operators
+# I think we would have to use comparison operators.
 
-depositAmount= 100.0
+name='james'
+passwword='246'
 
-savingsAccountBalance= 300.0
+securityName='james'
+securityPassword='246'
 
-print(depositAmount+ savingsAccountAlance)
+print (name== security and password== securityPassword)
