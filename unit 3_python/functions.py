076df9aeba8aebb26172915bd6bad4d88ecdbb11
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-11-20 13:47:12 +0000

    made new notes

--- unit 3_python/functions.py
+++ unit 3_python/functions.py
@@ -30,3 +30,53 @@ def pbAndj():
     print(' step 2. spread penut butter on one side of bread')
     print(' step 3. spread jelly on the other side of bread')
     print(' step 4. put sandwich together with spread/')
+
+
+
+    # functions 11/20/24
+
+    # function definition= tells the computer
+    # our instructions and stores it for later
+    # untl its called
+def performMath(x):
+       print('data has benn recieved.')
+       print('about to preform multiplication')
+       print(300*x)
+       print('task complete')
+       print('this function will now do addition')
+       print(26+x)
+
+# function call- when we write the funtions
+# name, the computer will runour
+# instructions
+performMath(2) # the number inside
+#the round bracket is the data
+# we want our functions to work on.
+performMath(14)
+# we want our function to work on.
+# performMath(14)
+# performMath(28)
+
+def depositMoney():
+# int() is a built in function
+# that allows us to transformstrings into numbers.
+# we pass in the data we want to transform into the 
+# round bracket
+    depositAmount= int (input('please enter the amount you want to deposit'))
+    checkingAccount= 2000
+    ('desposit put into account sucessfully.')
+    print(depositamount + checkingAccount)
+
+# depositMonye()
+
+# just like in a resturant, kitchen has the function
+# definition, but will only make the food 
+# when its called
+
+    
+
+
+
+
+       
+
