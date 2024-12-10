99b7af9fb45ec6b1b49a589409775a0c207fb077
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-12-10 13:39:31 +0000

    learned the code

--- unit 3_python/asyncActivityDec6.py
+++ unit 3_python/asyncActivityDec6.py
@@ -1,4 +1,17 @@
-def reverseWord ():
-    print(pet=tep)
+def reverseWord(word):
+    txt =  word [::-1]
+    print(txt)
 
-def reverseWord(pet)
+reverseWord('red')
+
+
+#
+
+def landmarkByState(state)
+    if state =='NY'
+        print('a landmark NY is the statue of Liberty')
+    elif state == 'PA':
+        print('a landmark in PA is the liberty bell')
+    elif state == 'California' 
+        print('a landmark in Califorina is the Hollywood Walk of Fame ')
+          
