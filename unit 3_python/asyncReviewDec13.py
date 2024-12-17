90c9db9d36315d17bbb749d561051e5d3e057159
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-12-17 13:47:06 +0000

    learned code

---
+++ unit 3_python/asyncReviewDec13.py
@@ -0,0 +1,9 @@
+def positiveOrNegavtive(number):
+    if number > -1:
+        print('This is a negative number')
+    elif number  < 0:
+        print('This is a negative number')
+    elif number == 0:
+        print('this is zero')
+    else:
+        print('number not recognized')         
