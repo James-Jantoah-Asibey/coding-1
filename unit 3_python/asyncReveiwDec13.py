9f77d1ddf2e9189d5ba34efe476591a97e978a0a
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-12-18 13:47:58 +0000

    learned code

---
+++ unit 3_python/asyncReveiwDec13.py
@@ -0,0 +1,27 @@
+# what is this question asking me to do
+# we need to make a discount software
+
+# we need the membership tier
+# the item name and prrice
+
+def shoppingDiscount(membership,itemName, itemPrice):
+    if membership== 'superShopper':
+        print('you will get 10 percent off your item')
+        discountAmount = itemPrice * 0.1
+        finalAmout = itemPrice -discountAmount
+        print('congrats supershopper! you will get 10 percent off your item')
+        print(finalAmount)
+    elif membership== 'megaShopper'
+        print('you will get 15 percent off your item')
+        discountAmount = itemPrice * 0.15
+        finalAmout = itemPrice -discountAmount
+        print(finalAmount)
+    elif membership== 'ultraShopper'
+        print('you will get 20 percent off our item')
+        discountAmount = itemPrice * 0.2
+        finalAmout = itemPrice -discountAmount
+        print(finalAmount)
+    else:
+        print('you do not have any membership benefits')
+
+ahoppinggDiscount('superShopper',100)            
--- unit 3_python/asyncReviewDec13.py
+++ unit 3_python/asyncReviewDec13.py
@@ -1,6 +1,6 @@
 def positiveOrNegavtive(number):
     if number > -1:
-        print('This is a negative number')
+       print('This is a negative number')
     elif number  < 0:
         print('This is a negative number')
     elif number == 0:
