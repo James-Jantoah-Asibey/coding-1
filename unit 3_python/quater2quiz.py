6ed321053617f6af0cfc252fc7794a3d032579b6
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2024-12-20 13:48:17 +0000

    tried the test

---
+++ unit 3_python/quater2quiz.py
@@ -0,0 +1,13 @@
+print('A function is a set of instructions that will perform a specific task. ')
+print('A function parameter is a variable inside of a parenthesis while an argument is a value of the functions when you call it ')
+print('IF were looking for a condition we can run or code, otherwise or ELSE it will doing something else on defult')
+print(' An ingeter data type are numerical data types')
+print('A boolean data type is when you run a condition in an if statement, Python returns True or False')
+print(' The oberator type i would use would be idneity operators to veriify if the person is old enough to puchsse te game')
+print('i would use the Arithmetic operators because we are dealing wiht numbers')
+def passwordLength(length):
+    if length== 10+:
+       print('sorry, password is to long')
+
+            
+
