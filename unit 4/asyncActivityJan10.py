d1eb63307d21370470ed2bfe89023176c1d363d0
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-01-10 13:46:25 +0000

    did the assignment but not finished

---
+++ unit 4/asyncActivityJan10.py
@@ -0,0 +1,6 @@
+frozenFoodIsle = ['icecream', 'cake', 'popsicles']
+internationalFoodIsle = ['samosas', 'pizza', 'sushi']
+produce = ['peppers', 'carrots', 'potatos']
+
+frozenFoodIsle = ['frozen fruit', 'icecream sandwitch', 'frozen fries']
+internationalFoodIsle =m[]
