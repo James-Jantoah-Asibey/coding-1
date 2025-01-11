050de7763b3c1431d1d0647b732caf75cfe493c2
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-01-11 04:47:52 +0000

    im done

--- unit 4/asyncActivityJan10.py
+++ unit 4/asyncActivityJan10.py
@@ -1,8 +1,20 @@
+# Question 1
+
 frozenFoodIsle = ['icecream', 'cake', 'popsicles', 'icecream sandwhich', 'frozen fries']
 internationalFoodIsle = ['samosas', 'pizza', 'sushi', 'noodles', 'curry']
 produce = ['peppers', 'carrots', 'potatos', 'onios', 'lettuce',]
 
+# Question 2 
 
+print("Number at index 0:", numbers=[0])
+print("Number at index 3:", numbers=[3])
+print("Number at index 4:", numbers=[4])
 
+# Question 3
 
+def multiplyIndex4(factor):
+    result == numbers[4]*factor
 
+ multiplyIndex4()   
+
+print('result of multiplying numbers at index 4 by 2:', multiplyIndex4(2))
