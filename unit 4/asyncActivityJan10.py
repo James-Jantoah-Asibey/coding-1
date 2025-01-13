b93c3caa0b971ff717ed9928acd3860d136b9e75
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-01-13 13:34:51 +0000

    did a review

--- unit 4/asyncActivityJan10.py
+++ unit 4/asyncActivityJan10.py
@@ -6,9 +6,9 @@ produce = ['peppers', 'carrots', 'potatos', 'onios', 'lettuce',]
 
 # Question 2 
 
-print("Number at index 0:", numbers=[0])
-print("Number at index 3:", numbers=[3])
-print("Number at index 4:", numbers=[4])
+print('Number at index 0:', numbers=[0])
+print('Number at index 3:', numbers=[3])
+print('Number at index 4:', numbers=[4])
 
 # Question 3
 
@@ -18,3 +18,27 @@ def multiplyIndex4(factor):
  multiplyIndex4()   
 
 print('result of multiplying numbers at index 4 by 2:', multiplyIndex4(2))
+
+
+# right answers
+
+# question 2
+
+# index 0
+
+1
+
+# index 3 
+50
+ 
+
+# index 4
+293
+
+# question 3 
+
+def multiplyNumber(x):
+    result= x * 293
+    print(result)
+
+multiplyNumber(5)    
---
+++ unit 4/asyncReviewJan10.py
@@ -0,0 +1,6 @@
+# method =  a method is a function that works with other functions.
+
+travelList = ['california', 'florida', 'atlantic city', 'atlanta', 'georgia']
+
+# the insert function is used to add a element to a list
+# the insert function is a built-in function - meaning
