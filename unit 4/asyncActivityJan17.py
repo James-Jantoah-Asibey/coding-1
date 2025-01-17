5c384fe79070a50ca2974a36af810b1fb38853a8
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-01-17 13:41:06 +0000

    Did the work

---
+++ unit 4/asyncActivityJan17.py
@@ -0,0 +1,21 @@
+# Question 1
+
+campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+campingSupplies.sort()
+
+print(campingSupplies)
+
+# Question 2
+
+campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+campingSupplies.append("lighter")
+
+print(campingSupplies)
+
+# Question 3
+
+campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+campingFood = ['marshmellows','gram crackers','chocolate','chicken hot dogs','water',]
+
+list3 = campingFood + campingSupplies 
+print(list3)
