d9f6071eb6e182dfed268727ca86cbfda199a88e
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-01-23 13:46:24 +0000

    did some work

--- unit 4/whileLoops.py
+++ unit 4/whileLoops.py
@@ -22,7 +22,22 @@ def tripSavings():
      accountBalance = 0 
      tripGoal = 8000
      while accountBalance < tripGoal:
-          depositAmount = input('How much you want to despoit ?')
-          newBalance = int(deositAmount) + accountBalance 
-          print('your new balance is '+ str(newBalance))
-     
+          depositAmount =  int (input('How much you want to despoit ?'))
+          accountBalance = depositAmount + accountBalance 
+          print('your new balance is '+ str(accountBalance))
+
+tripSavings() 
+
+def damageCounter():
+    player1 = 100
+    while player1 > 0:
+        damage = input("How much damage is this attack doing? :")
+        player1 -= damage
+        print('Player1 health is now 'str(player1) +'healthpoints')
+
+damageCounter()
+
+
+def verifyPassword(password):
+
+
