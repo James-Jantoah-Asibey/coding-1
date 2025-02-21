ce09b114281234ff2395114d3433e78ac5881c74
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-02-21 13:41:54 +0000

    did the work

---
+++ unit 4/asyncActivityFeb21.py
@@ -0,0 +1,9 @@
+" the name of my game is called bullseye"
+
+"the genre of my game is shooting"
+
+"players will shoot random targets that appear on the map with a given time, points will differ from where on the target they shoot"
+
+"my game takes inspiration from the GamePigeon games which are: Darts: which the closer you get to the middle the more points you get, Archery:the zoom in and out feature to allow the player to get a better shot, and minigolf: the powerups you can get to either slow down time or increase the target size to get a better shot"
+
+"my game will alow the player to zoom in and out to get a better shot of the target. it will also allow the player to gain powerups to help them shoot the target, like to slow down time or multiply their points when they hit the target"
